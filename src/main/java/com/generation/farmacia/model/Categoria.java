b8532ec881e5fa4d126f99e38eb877b03319cac2
gustavoalrib <gustavoalrib@gmail.com>
2024-01-31 11:53:49 -0300

    criado crud de categoria, testado e validado

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,38 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo classe é obrigatório.")
+	@Size(min = 3, max = 50, message = "O atributo classe deve conter no mínimo 3 caracteres e no máximo 50 caracteres.")
+	private String classe;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getClasse() {
+		return classe;
+	}
+
+	public void setClasse(String classe) {
+		this.classe = classe;
+	}
+}
