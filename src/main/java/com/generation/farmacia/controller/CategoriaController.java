714cd562e7c6d95dda428900654b07f1e2083153
gustavoalrib <gustavoalrib@gmail.com>
2024-01-31 14:19:42 -0300

    ajuste de nomes de variáveis

--- src/main/java/com/generation/farmacia/controller/CategoriaController.java
+++ src/main/java/com/generation/farmacia/controller/CategoriaController.java
@@ -62,9 +62,9 @@ public class CategoriaController {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable Long id) {
-		Optional<Categoria> tema = categoriaRepository.findById(id);
+		Optional<Categoria> categoria = categoriaRepository.findById(id);
 
-		if (tema.isEmpty())
+		if (categoria.isEmpty())
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
 		categoriaRepository.deleteById(id);
